cce77508014b286d71d2cbbe069df471915b8f89
itzabhiai <141840566+itzabhiai@users.noreply.github.com>
2024-08-09 14:58:41 +0530

    Update index.js
    squernest

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 1000;
 
 // Use the cors middleware to allow cross-origin requests
 app.use(cors({
-  origin: 'https://asquerenest.netlify.app/', 
+  origin: 'https://asquarenest.com', 
   methods: ['GET', 'POST'],
 }));
 
